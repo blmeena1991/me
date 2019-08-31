9d5317e673d3fd070023aedf9bfc122e3544a0f7
blmeena <babulal@bizongo.in>
2019-08-31 12:22:30 +0530

    Updates

---
+++ precache-manifest.66dfe148637677d3be0987b4c129728d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49b85e2810e02c5b2a3b6ba4c9fe919c",
+    "url": "/me/index.html"
+  },
+  {
+    "revision": "ccc611fbbadc1e58a759",
+    "url": "/me/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "cc95cf52ff9118fd5293",
+    "url": "/me/static/js/2.51678b07.chunk.js"
+  },
+  {
+    "revision": "ccc611fbbadc1e58a759",
+    "url": "/me/static/js/main.62344e0d.chunk.js"
+  },
+  {
+    "revision": "515b16f1f0353ed3628d",
+    "url": "/me/static/js/runtime~main.5422a9c8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/me/static/media/logo.5d5d9eef.svg"
+  }
+]);
